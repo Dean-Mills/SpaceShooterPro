47de4e9d2156ede63fa56a3c0d178b6c7a5357d4
Dean Mills <deansmills@gmail.com>
2020-10-04 18:13:51 +0200

    clamping y movement nicely

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -14,23 +14,21 @@ public class Player : MonoBehaviour
 
     void Update()
     {
+        CalculateMovement();
+    }
+
+    void CalculateMovement()
+    {
         var horizontal = Input.GetAxis("Horizontal");
         var vertical = Input.GetAxis("Vertical");
         var direction = new Vector3(horizontal, vertical, 0).normalized;
-        transform.Translate(direction  * _speed *  Time.deltaTime);
-        if(transform.position.y >= 0)
-        {
-            transform.position = new Vector3(transform.position.x, 0, 0);
-        }else if(transform.position.y <= -3.8)
-        {
-            transform.position = new Vector3(transform.position.x, -3.8f, 0);
-        }
-
+        transform.Translate(direction * _speed * Time.deltaTime);
+        transform.position = new Vector3(transform.position.x, Mathf.Clamp(transform.position.y, -3.8f, 0f), 0);
         if (transform.position.x >= 11.3f || transform.position.x <= -11.3f)
         {
             //bug here that if player stays at the edge the player will oscillate from one side to the other
             //will have to fix this at some point 
-            transform.position = new Vector3(-1*transform.position.x, transform.position.y, 0);
+            transform.position = new Vector3(-1 * transform.position.x, transform.position.y, 0);
         }
     }
 }
