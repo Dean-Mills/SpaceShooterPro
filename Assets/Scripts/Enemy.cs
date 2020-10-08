c663530b6819fc945c8754821442543d4bead43e
Dean Mills <deansmills@gmail.com>
2020-10-08 22:51:48 +0200

    Added falling enemies and triggers

---
+++ Assets/Scripts/Enemy.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    [SerializeField]
+    private float _speed = 4f;
+    void Update()
+    {   
+        transform.Translate(Vector3.down * _speed * Time.deltaTime);
+        if(transform.position.y < -4.5f)
+        {
+            var randomX = Random.Range(-10f, 10f);
+            var randomY = Random.Range(5.5f, 6f);
+            transform.position = new Vector3(randomX, randomY, 0f);
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        Debug.Log(other.tag);
+        if(other.tag == "Laser")
+        {
+            Destroy(this.gameObject);
+            Destroy(other.gameObject);
+        }
+        if(other.tag == "Player")
+        {
+            Destroy(this.gameObject);
+        }
+    }
+}
